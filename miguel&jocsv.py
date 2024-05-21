e3f1e5984ca2b1869d703e022f4562443dd40c40
migueelmartinezzz <maartinez.miguel476@gmail.com>
2024-05-21 15:28:37 -0300

    Update miguel&jocsv.py

--- miguel&jocsv.py
+++ miguel&jocsv.py
@@ -3,6 +3,7 @@ import streamlit as st
 import matplotlib.pyplot as plt
 df=pd.read_csv('songs_normalize.csv')
 df['popularity']
+st.set_option('deprecation.showPyplotGlobalUse', False)
 st.title('Histograma de Popularidade de Músicas')
 st.write("Histograma da popularidade das músicas:")
 plt.hist(df['popularity'])
